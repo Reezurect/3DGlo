d31562c77485e89205856d79a910de0e4894256b
Denis Zudilin <zudilin2014@yandex.ru>
2023-11-10 17:10:49 +0300

    lesson21

--- lesson19/src/index.js
+++ lesson19/src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validation from './modules/validation'
 
 timer("10 november 2023")
 menu()
 modal()
+validation()
