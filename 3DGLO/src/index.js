7f6748491e2d424d7be9e54422f15af71a5f896e
Denis Zudilin <zudilin2014@yandex.ru>
2023-11-11 15:12:46 +0300

    lesson22доделал

--- 3DGLO/src/index.js
+++ 3DGLO/src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import validation from './modules/validation'
+import tabs from './modules/tabs'
 
 timer("10 november 2023")
 menu()
 modal()
 validation()
+tabs()
