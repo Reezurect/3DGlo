82a9559e9c5b9d5b5c9723588e4b4e75bfc3135f
Denis Zudilin <zudilin2014@yandex.ru>
2023-11-11 16:18:12 +0300

    lesson23

--- 3DGLO/src/index.js
+++ 3DGLO/src/index.js
@@ -3,9 +3,13 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
+import dots from './modules/dots'
 
 timer("10 november 2023")
 menu()
 modal()
 validation()
 tabs()
+dots()
+slider(".portfolio-content", ".portfolio-item", ".dot")
