a88a9125c4b92746b4cc5283f5080d3e4eb84b7b
Denis Zudilin <zudilin2014@yandex.ru>
2023-11-18 17:01:00 +0300

    lesson27

--- 3DGLO/src/index.js
+++ 3DGLO/src/index.js
@@ -6,6 +6,7 @@ import tabs from './modules/tabs'
 import slider from './modules/slider'
 import dots from './modules/dots'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
 
 timer("10 november 2023")
 menu()
@@ -15,3 +16,14 @@ tabs()
 dots()
 slider(".portfolio-content", ".portfolio-item", ".dot")
 calc(100)
+sendForm({
+    formId_1: 'form1',
+    formId_2: 'form2',
+    formId_3: 'form3',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
